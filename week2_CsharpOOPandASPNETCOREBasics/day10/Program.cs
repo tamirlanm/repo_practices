61148c5d5086a6f7bba49748f3427a34cb239a42
tamirlanm <tamirlanmirgali@gmail.com>
2026-03-27 10:14:02 +0500

    linq base

--- week2_CsharpOOPandASPNETCOREBasics/day10/Program.cs
+++ week2_CsharpOOPandASPNETCOREBasics/day10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] people = {"Tom", "Bob", "Sam", "Tomas"};
+
+var selectedPeople = from p in people where p.ToUpper().StartsWith('T') orderby p select p;
+
+foreach(var per in selectedPeople)
+{
+    Console.WriteLine(per);
+}
+
+int num = (from p in people where p.ToUpper().StartsWith('T') select p).Count();
+Console.WriteLine(num);
