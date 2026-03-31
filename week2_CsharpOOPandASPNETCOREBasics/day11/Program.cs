220d7447b567dcc3f120151b8f4912f4671afd06
tamirlanm <tamirlanmirgali@gmail.com>
2026-03-31 20:28:02 +0500

    asynchronous programming example

---
+++ week2_CsharpOOPandASPNETCOREBasics/day11/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task<string> GetUserName()
+    {
+        await Task.Delay(1500);
+        return "Johnson";
+    }
+
+    
+    static async Task<string> ReadingData()
+    {
+        await Task.Delay(2000);
+        return "Data is uploaded";
+    }
+
+    static async Task Main()
+    {
+        Console.WriteLine("Getting you'r name...");
+
+        string[] result = await Task.WhenAll(GetUserName(),ReadingData());
+        Console.WriteLine(result[0]);
+        Console.WriteLine(result[1]);
+
+
+        /*       
+        string name = await GetUserName();
+        string res = await ReadingData();
+        Console.WriteLine(res);
+        Console.WriteLine($"Finally {name}");
+        */
+    }
+
+
+}
