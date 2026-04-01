24adeca52448b290b5418b39595f4917dac98869
tamirlanm <tamirlanmirgali@gmail.com>
2026-04-01 17:21:18 +0500

    basics_of_asynchronous_programming

--- week2_CsharpOOPandASPNETCOREBasics/day11/Program.cs
+++ week2_CsharpOOPandASPNETCOREBasics/day11/Program.cs
@@ -1,37 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿var tomTask = PrintNameAsync("Tom");
+var marcusTask = PrintNameAsync("Marcus");
+var anissaTask = PrintNameAsync("Anissa");
 
-class Program
+await tomTask;
+await marcusTask;
+await anissaTask;
+
+async Task PrintNameAsync(string name)
 {
-    static async Task<string> GetUserName()
-    {
-        await Task.Delay(1500);
-        return "Johnson";
-    }
-
-    
-    static async Task<string> ReadingData()
-    {
-        await Task.Delay(2000);
-        return "Data is uploaded";
-    }
-
-    static async Task Main()
-    {
-        Console.WriteLine("Getting you'r name...");
-
-        string[] result = await Task.WhenAll(GetUserName(),ReadingData());
-        Console.WriteLine(result[0]);
-        Console.WriteLine(result[1]);
-
-
-        /*       
-        string name = await GetUserName();
-        string res = await ReadingData();
-        Console.WriteLine(res);
-        Console.WriteLine($"Finally {name}");
-        */
-    }
-
-
+    await Task.Delay(3000);
+    Console.WriteLine(name);
 }
---
+++ week2_CsharpOOPandASPNETCOREBasics/day12/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Message mes = Hello;
+mes();
+Operation op = Add;
+int n = op(3, 4);
+Console.WriteLine(n);
+ 
+void Hello() => Console.WriteLine("Hello");
+int Add(int x, int y) => x + y;
+ 
+delegate int Operation(int x, int y);
+delegate void Message();
+*/
+MessageHandler handler = delegate
+{
+    Console.WriteLine("анонимный метод");
+};
+handler("hello world!");    // анонимный метод
+ 
+delegate void MessageHandler(string message);
